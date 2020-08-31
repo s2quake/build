a459e6bfd9907dc7784f364105aa0df6bffaf0d9
s2quake <han0210@netsgo.com>
2020-08-31 18:31:31 +0900

    샘플 파일 수정

--- build-sample.sh
+++ build-sample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pwsh -executionpolicy remotesigned -File $(pwd)/$(dirname $0)/build-sample.ps1 -AssemblyOriginatorKeyFile "$@"
+pwsh -executionpolicy remotesigned -File $(pwd)/$(dirname $0)/build-sample.ps1 "$@"
